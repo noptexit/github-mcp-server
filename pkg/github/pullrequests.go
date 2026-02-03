fb53abdbe2f734f5d4e2b686bdac448e8e043048
Roberto Nacu <kerobbi@github.com>
2026-02-03 15:42:14 +0000

    replace context.Background() (#1948)

--- pkg/github/pullrequests.go
+++ pkg/github/pullrequests.go
@@ -1502,7 +1502,7 @@ func SubmitPendingPullRequestReview(ctx context.Context, client *githubv4.Client
 		"prNum":  githubv4.Int(params.PullNumber),
 	}
 
-	if err := client.Query(context.Background(), &getLatestReviewForViewerQuery, vars); err != nil {
+	if err := client.Query(ctx, &getLatestReviewForViewerQuery, vars); err != nil {
 		return ghErrors.NewGitHubGraphQLErrorResponse(ctx,
 			"failed to get latest review for current user",
 			err,
@@ -1587,7 +1587,7 @@ func DeletePendingPullRequestReview(ctx context.Context, client *githubv4.Client
 		"prNum":  githubv4.Int(params.PullNumber),
 	}
 
-	if err := client.Query(context.Background(), &getLatestReviewForViewerQuery, vars); err != nil {
+	if err := client.Query(ctx, &getLatestReviewForViewerQuery, vars); err != nil {
 		return ghErrors.NewGitHubGraphQLErrorResponse(ctx,
 			"failed to get latest review for current user",
 			err,
@@ -1761,7 +1761,7 @@ func AddCommentToPendingReview(t translations.TranslationHelperFunc) inventory.S
 				"prNum":  githubv4.Int(params.PullNumber),
 			}
 
-			if err := client.Query(context.Background(), &getLatestReviewForViewerQuery, vars); err != nil {
+			if err := client.Query(ctx, &getLatestReviewForViewerQuery, vars); err != nil {
 				return ghErrors.NewGitHubGraphQLErrorResponse(ctx,
 					"failed to get latest review for current user",
 					err,
